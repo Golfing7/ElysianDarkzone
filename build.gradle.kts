539e36ae43369ce4b6aedede54b353b6770c6898
Golfing7 <atroo@me.com>
2025-05-03 09:34:08 -0400

    Add kotlin runtime to package

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     kotlin("jvm") version "1.9.23"
+    id("com.gradleup.shadow") version("8.3.6")
 }
 
 val libraryFolder = "locallibs"
@@ -35,10 +36,10 @@ kotlin {
 }
 
 tasks.create("deploy") {
-    dependsOn(tasks.jar)
+    dependsOn(tasks.shadowJar)
 
     doFirst {
-        val outputFile = tasks.getByName("jar").outputs.files.first()
+        val outputFile = tasks.getByName("shadowJar").outputs.files.first()
         val targetFile = File(deployDirectory, "${project.name}-${project.version}.jar")
 
         outputFile.copyTo(targetFile, overwrite = true)
